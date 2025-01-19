a50a224f44d4c4db2ef88c6d9fa08145b1d4498c
Younus <you@example.com>
2025-01-19 12:41:35 +0530

    Responsive 3

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,9 +10,7 @@ export default function NavBar() {
     const [isExpanded, setIsExpanded] = useState(false);
     const location = useLocation();
 
-    const handleToggle = () => {
-        setIsExpanded(!isExpanded);
-    };
+
 
     const handleLogout = () => {
         localStorage.removeItem("authToken");
